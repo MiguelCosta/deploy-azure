80acb04814a0c939c88acf56a5088a7667d4ba5a
Miguel Costa <miguelpintodacosta@gmail.com>
2018-08-04 15:12:55 +0100

    Add project files.

---
+++ Mpc.DeployAzure.WebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Mpc.DeployAzure.WebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
